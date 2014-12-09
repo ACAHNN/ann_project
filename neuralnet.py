ee806840c020f8f1904203f3f0e9c0763cc3e3e1
Aaron Cahn <cahn@optimus.cs.wisc.edu>
2014-12-09 13:47:16 -0600

    initial commit of the neuralnet code base

---
+++ neuralnet.py
@@ -0,0 +1,17 @@
+import sys
+import random
+import numpy as np
+
+
+
+class NeuralNet:
+
+    def __init__(self, inputs, hiddens, outputs):
+        pass
+    
+    def _weight_matrix(self, rows, columns):
+        pass
+
+    
+if __name__ == '__main__':
+    
