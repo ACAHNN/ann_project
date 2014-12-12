38c52d9e225ac83d670c3168807b0bbd6c777255
acahn <acahn22@gmail.com>
2014-12-12 01:23:38 -0600

    implemented a fully generalized neuralnet framework. The code base is now capable of handling any number of hidden layers and any number of nodes at each layer. Todo: implement the deep learning procedure to better learn hidden layer weights.

--- neuralnet.py
+++ neuralnet.py
@@ -15,19 +15,21 @@ class NeuralNet:
         
         # activation of nodes
         self.input_activations = np.reshape([1]*self.num_in, (1,self.num_in))
-        self.hidden_activations = np.reshape([1]*self.num_hidden, (1,self.num_hidden))
+        self.hidden_activations = [np.reshape([1]*i, (1,i)) for i in self.num_hidden]
         self.output_activations = np.reshape([1]*self.num_out, (1,self.num_out))
         
         # errors of nodes
-        self.hidden_errors = np.reshape([1]*self.num_hidden, (1,self.num_hidden))
+        self.hidden_errors = [np.reshape([1]*i, (1,i)) for i in self.num_hidden]
         self.output_errors = np.reshape([1]*self.num_out, (1,self.num_out))
         
         # learn rate of the neuralnet
         self.learn_rate = learn_rate
         
         # weight matrices
-        self.input_weights = self._create_weight_matrix(self.num_in, self.num_hidden)
-        self.hidden_weights = self._create_weight_matrix(self.num_hidden, self.num_out)
+        self.input_weights = self._create_weight_matrix(self.num_in, self.num_hidden[0]) # input->hidden layer 1
+        self.hidden_weights = [self._create_weight_matrix(i, j) for i, j in zip(self.num_hidden, self.num_hidden[1:])] # hidden->hidden layers
+        self.output_weights = self._create_weight_matrix(self.num_hidden[-1], self.num_out) # last hidden->output layer
+
 
     def sigmoid(self, x):
         return 1/(1+math.exp(-x))
@@ -36,32 +38,48 @@ class NeuralNet:
         # append bias term
         self.input_activations = np.array(np.append(d,1)).reshape(1,self.num_in)
 
-        # input->hidden layer
-        inets = np.dot(self.input_activations, self.input_weights)
-        self.hidden_activations = np.array(map(np.vectorize(self.sigmoid), inets)).reshape(1,self.num_hidden)
+        # input->hidden layer 1
+        ihnets = np.dot(self.input_activations, self.input_weights)
+        self.hidden_activations[0] = np.array(map(np.vectorize(self.sigmoid), ihnets)).reshape(1,self.num_hidden[0])
         
-        # hidden->output layer
-        hnets = np.dot(self.hidden_activations, self.hidden_weights)
-        self.output_activations = np.array(map(np.vectorize(self.sigmoid), hnets)).reshape(1,self.num_out)
+        # hidden->hidden layers
+        for i in xrange(len(self.hidden_weights)):
+            hhnets = np.dot(self.hidden_activations[i], self.hidden_weights[i])
+            self.hidden_activations[i+1] = np.array(map(np.vectorize(self.sigmoid), hhnets)).reshape(1,self.num_hidden[i+1])
+        
+        # last hidden->output layer
+        honets = np.dot(self.hidden_activations[-1], self.output_weights)
+        self.output_activations = np.array(map(np.vectorize(self.sigmoid), honets)).reshape(1,self.num_out)
 
     
     def backpropagate(self, y):
-        # calculate hidden->output errors
+        # calculate output errors
         self.output_errors = self.output_activations \
             *(1-self.output_activations) \
             *(y-self.output_activations)
         
-        # calculate input->hidden errors
-        self.hidden_errors = self.hidden_activations \
-            *(1-self.hidden_activations) \
-            *(np.transpose((np.dot(self.hidden_weights, np.transpose(self.output_errors)))))
+        # calculate last hidden layer errors
+        self.hidden_errors[-1] = self.hidden_activations[-1] \
+            *(1-self.hidden_activations[-1]) \
+            *(np.transpose((np.dot(self.output_weights, np.transpose(self.output_errors)))))
         
-        # update hidden->output weight matrix
-        dhoweights = self.learn_rate*(np.dot(np.transpose(self.hidden_activations), self.output_errors))
-        self.hidden_weights += dhoweights
+        # calculate n-1 hidden layer errors
+        for i in range(len(self.hidden_errors)-2,-1,-1):
+            self.hidden_errors[i] = self.hidden_activations[i] \
+                *(1-self.hidden_activations[i]) \
+                *(np.transpose((np.dot(self.hidden_weights[i], np.transpose(self.hidden_errors[i+1])))))
+
+        # update last hidden->output weight matrix
+        dhoweights = self.learn_rate*(np.dot(np.transpose(self.hidden_activations[-1]), self.output_errors))
+        self.output_weights += dhoweights
+        
+        # update hidden->hidden weight matrix
+        for i in xrange(len(self.hidden_weights)):
+            dhhweights = self.learn_rate*(np.dot(np.transpose(self.hidden_activations[i]), self.hidden_errors[i+1]))
+            self.hidden_weights[i] += dhhweights
         
         # update input->hidden weight matrix
-        dihweights = self.learn_rate*(np.dot(np.transpose(self.input_activations), self.hidden_errors))
+        dihweights = self.learn_rate*(np.dot(np.transpose(self.input_activations), self.hidden_errors[0]))
         self.input_weights += dihweights
     
     
@@ -72,27 +90,50 @@ class NeuralNet:
         return m
     
     def print_weights(self):
+        # input and output weight matrices
         s = "\ninput->hidden weight matrix:\n%s\n" +\
+            "%s" +\
             "\nhidden->output weight matrix:\n%s\n"
-        print s % (self.input_weights, self.hidden_weights)
+        
+        # n hidden layer matrices
+        tmp = ""
+        for i in xrange(len(self.hidden_weights)):
+            tmp += "\nhidden->hidden weight matrix:\n" +\
+                   "%s\n" % self.hidden_weights[i]
+        
+        print s % (self.input_weights, tmp, self.output_weights)
         
     def print_activations(self):
+        # input and output activations
         s = "\ninput activations:\n%s " +\
-            "\nhidden activations:\n%s " +\
+            "%s" +\
             "\noutput activations:\n%s\n"
+        
+        # n hidden layer activations
+        tmp = ""
+        for i in xrange(len(self.hidden_activations)):
+            tmp += "\nhidden activations:\n%s " % self.hidden_activations[i]
+        
         print s % (self.input_activations,
-                   self.hidden_activations,
+                   tmp,
                    self.output_activations)
+    
     def print_errors(self):
+        # output layer errors
         s = "\noutput errors:\n%s " +\
-            "\nhidden errors:\n%s "
+            "%s\n"
+        
+        # n hidden layers errors
+        tmp = ""
+        for i in xrange(len(self.hidden_errors)-1,-1,-1):
+            tmp += "\nhidden errors:\n%s " % self.hidden_errors[i]
+        
         print s % (self.output_errors,
-                   self.hidden_errors)
+                   tmp)
                    
     def train(self, data, target, epochs):
         for i in xrange(epochs):
             for instance in data:
-                #print instance
                 self.feed(instance[0]) # X's
                 self.backpropagate(instance[1]) # Y's
     
@@ -116,15 +157,16 @@ class NeuralNet:
 
 if __name__ == '__main__':
     ### TEST DATA ###
-    data = [(np.array([1,1]).reshape(1,2),np.array([1]).reshape(1,1)),
+    data = [(np.array([1,1]).reshape(1,2),np.array([0]).reshape(1,1)),
             (np.array([1,0]).reshape(1,2),np.array([1]).reshape(1,1)),
             (np.array([0,1]).reshape(1,2),np.array([1]).reshape(1,1)),
-            (np.array([0,0]).reshape(1,2),np.array([1]).reshape(1,1))]
+            (np.array([0,0]).reshape(1,2),np.array([0]).reshape(1,1))]
+#    data = [(np.array([1,0]).reshape(1,2),np.array([1,1]).reshape(1,2))]
     ################
 
-    nn = NeuralNet(2, 3, 1, .1)
-    nn.train(data, None, 1000)
-    nn.test(data, None, False)
+    nn = NeuralNet(2, [4], 1, .1)
+    nn.train(data, None, 1000000)
+    nn.test(data, None, True)
     
     #nn.print_activations()
     #nn.print_errors()
