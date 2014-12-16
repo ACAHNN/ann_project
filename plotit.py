d7f0656d4c38679b6fd031cceb77cc647c086786
Aaron Cahn <cahn@optimus.cs.wisc.edu>
2014-12-15 18:26:07 -0600

    adding paper modifications

--- plotit.py
+++ plotit.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as path_effects
-from utils import *
-
+import cPickle as pickle
+import numpy as np
 
 if __name__ == "__main__":
-    desc, data = qload("wbcd_results_full_matrix.pkl")
+    desc, data = pickle.load(open("data/wbcd_results_full_matrix.pkl"))
     print desc
-    fig = plt.figure(figsize = (10, 10))
+    fig = plt.figure(figsize = (10, 4))
     ax = fig.add_subplot(111)
     cmap = plt.get_cmap("jet")
     
-    c = ax.imshow(data, interpolation = "none", cmap = cmap, aspect = 0.6)
+    c = ax.imshow(data, interpolation = "none", cmap = cmap, aspect = 0.4, origin='bottom')
     #cmap = c.get_cmap()
     print type(c.to_rgba(90))
 
@@ -22,15 +22,15 @@ if __name__ == "__main__":
     min_val, max_val = 0, data.shape[1] 
     ind_array2 = np.arange(min_val, max_val + 0.0, 1.0)
     
-    x, y = np.meshgrid(ind_array2, ind_array1)
+    x, y = np.meshgrid(ind_array1, ind_array2)
     print x.shape, y.shape, data.shape
     rng = np.max(data) - np.min(data)
     for i, (x_val, y_val) in enumerate(zip(x.flatten(), y.flatten())):
         #c = 'x' if i%2 else 'o' 
-        val = data[y_val, x_val]
+        val = data[x_val, y_val]
         label = "%.2f" % val
         cval = -((val - np.min(data)) / 3.0) + np.max(data)
-        text = ax.text(x_val, y_val, label, va='center', ha='center', color = 'white')
+        text = ax.text(y_val, x_val, label, va='center', ha='center', color = 'white')
         text.set_path_effects([path_effects.Stroke(linewidth=1, foreground='black'),
                                path_effects.Normal()])
     plt.savefig("out.pdf")
